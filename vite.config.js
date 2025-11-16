eac6c59fcf63a7c0ae7d31c0a9207c1990c4ef59
Punnawit KOTOSRI <punnawitkotosri@192.168.1.40>
2025-11-16 16:37:15 +0700

    fix image path for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-        base: '/PK6-Portfolio/', 
-      },
-    }),
-  ],
+  plugins: [react()],
+  base: '/PK6-Portfolio/',   
 })
